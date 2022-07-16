d5af3fc18801f6dce1cce658ecd936e919f04cd4
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-07-16 19:53:56 +0700

    fixing env.ts errorMessage handler

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import fs from 'fs'
 
 import { isNumb, isString } from '@lib/validator'
 
@@ -8,10 +9,18 @@ export interface iEnv {
 }
 
 const env = (): iEnv => {
+  const errMsg = 'Please follow https://github.com/mnrendra/create-nodets-app#3-set-environment step.'
+
+  try {
+    fs.readFileSync('.env', { encoding: 'utf8', flag: 'r' })
+  } catch (e) {
+    throw new Error(`Need a .env file! ${errMsg}`)
+  }
+
   const { env } = process
 
-  if (env.PORT === undefined || !isNumb(Number(env.PORT))) throw new Error('Need a PORT on .env file!')
-  if (env.DB_URL === undefined || !isString(env.DB_URL) || env.DB_URL === '') throw new Error('Need a DB_URL on .env file!')
+  if (env.PORT === undefined || !isNumb(Number(env.PORT))) throw new Error(`Need a PORT on .env file! ${errMsg}`)
+  if (env.DB_URL === undefined || !isString(env.DB_URL) || env.DB_URL === '') throw new Error(`Need a DB_URL on .env file! ${errMsg}`)
 
   return {
     PORT: Number(env.PORT),
