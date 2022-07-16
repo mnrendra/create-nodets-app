795f0d63cb419734d48a26760d765d2c8d19d73f
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-07-16 19:59:45 +0700

    call main app.ts via main index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
-import 'module-alias/register';
+import 'module-alias/register'
 
-import express from 'express'
+import app from './app'
 
-import { env } from '@config'
-import db from '@db'
-import routes from '@routes'
-
-const main = async (): Promise<any> => {
-  // conf
-  const { PORT, DB_URL } = env()
-
-  // db
-  const dbMsg = await db(DB_URL, {})
-  console.log('dbMsg', dbMsg)
-
-  // app
-  const app = express()
-  app.use(express.json())
-  app.use(routes)
-  app.listen(PORT, () => console.log(`listen on port ${PORT}`))
-}
-
-main()
+app()
   .then(() => {
     console.log('Ready!')
   })
