2d214d925b1cf23ac6c8bbd11cae9b82a0c3e4c3
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-07-29 03:36:14 +0700

    update grpc client.

--- src/interfaces/grpc/example-client.ts
+++ src/interfaces/grpc/example-client.ts
@@ -7,30 +7,40 @@ const protoPath = './src/interfaces/grpc/protos/services.proto'
 const packageDefinition = protoLoader.loadSync(protoPath, protoOption)
 const { Example }: any = grpc.loadPackageDefinition(packageDefinition).services
 
+const EXAMPLE_GET_QUERY = {} // replace me!
+const EXAMPLE_GET_PARAMS = { id: 'example-get-param-id' } // replace me!
+
+const EXAMPLE_POST_BODY = { name: 'Example Post Name' } // replace me!
+
+const EXAMPLE_PUT_PARAMS = { id: 'example-put-params-id' } // replace me!
+const EXAMPLE_PUT_BODY = { name: 'Example Put Name' } // replace me!
+
+const EXAMPLE_DELETE_PARAMS = { id: 'example-delete-params-id' } // replace me!
+
 const main = (port: number = 50051): void => {
   const client = new Example(`localhost:${port}`, grpc.credentials.createInsecure())
 
-  client.getMany({ query: {} }, (err: any, response: any): void => {
+  client.getMany({ query: EXAMPLE_GET_QUERY }, (err: any, response: any): void => {
     if (err !== null && err !== undefined) throw new Error(err)
     console.log('getMany', response)
   })
 
-  client.getOne({ params: { id: '62e2e9c9c85d720a10d6aa33' } }, (err: any, response: any): void => {
+  client.getOne({ params: EXAMPLE_GET_PARAMS }, (err: any, response: any): void => {
     if (err !== null && err !== undefined) throw new Error(err)
     console.log('getOne', response)
   })
 
-  client.postOne({ body: { name: 'Foxtrot' } }, (err: any, response: any): void => {
+  client.postOne({ body: EXAMPLE_POST_BODY }, (err: any, response: any): void => {
     if (err !== null && err !== undefined) throw new Error(err)
     console.log('postOne', response)
   })
 
-  client.putOne({ params: { id: '62e2e16d70ebc9a93593aec1' }, body: { name: 'Echo One' } }, (err: any, response: any): void => {
+  client.putOne({ params: EXAMPLE_PUT_PARAMS, body: EXAMPLE_PUT_BODY }, (err: any, response: any): void => {
     if (err !== null && err !== undefined) throw new Error(err)
     console.log('putOne', response)
   })
 
-  client.delOne({ params: { id: '62e2e9c9c85d720a10d6aa33' } }, (err: any, response: any): void => {
+  client.delOne({ params: EXAMPLE_DELETE_PARAMS }, (err: any, response: any): void => {
     if (err !== null && err !== undefined) throw new Error(err)
     console.log('delOne', response)
   })
